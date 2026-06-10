8a9af01e04afc712e0ff3a40065af8134ce6c3a3
Lumirelle <shabbyacc@outlook.com>
2026-06-11 00:41:54 +0800

    feat!: udpate my neovim config

--- dot_config/shared/nvim/lua/config/keymaps.lua
+++ dot_config/shared/nvim/lua/config/keymaps.lua
@@ -1,5 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
-if vim.fn.executable("btop") == 1 then
-  -- btop
-  vim.keymap.set("n", "<leader>xb", function() Snacks.terminal.open({ "btop" }, { esc_esc = false, ctrl_hjkl = false }) end, { desc = "btop" })
-end
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
