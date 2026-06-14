9c226483f99f356816f9b01fe5bd29e5f23cf8c4
Lumirelle <shabbyacc@outlook.com>
2026-06-14 22:19:47 +0800

    feat: use catppucin as neovim theme

--- dot_config/shared/nvim/lua/config/options.lua
+++ dot_config/shared/nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Gui font, used by Neovide
 vim.o.guifont = "0xProto, Symbols Nerd Font, Maple Mono WD CN"
 
 -- Set default shell to Nushell, to get the cross-platform experience
@@ -15,3 +16,7 @@ vim.g.loaded_perl_provider = 0
 
 -- Enable ESLint auto format
 vim.g.lazyvim_eslint_auto_format = true
+
+-- Neovide specific config
+vim.g.neovide_fullscreen = true
+vim.g.neovide_title_background_color = "#1f2430"
