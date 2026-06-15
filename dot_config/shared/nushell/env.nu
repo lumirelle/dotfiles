522c96c74106cf52da0aba0914dd3f95627389cf
Lumirelle <shabbyacc@outlook.com>
2026-06-15 11:35:35 +0800

    feat: use zoxide globally via mise, drop tochezmoi alias

--- dot_config/shared/nushell/env.nu
+++ dot_config/shared/nushell/env.nu
@@ -3,14 +3,10 @@ mkdir ($nu.data-dir | path join "vendor/autoload")
 starship init nu | save -f ($nu.data-dir | path join "vendor/autoload/starship.nu")
 
 # Environment
-if (which chezmoi | is-not-empty) {
-  $env.CHEZMOI_HOME = (chezmoi source-path)
-}
 mise activate nu | save -f ($nu.data-dir | path join "vendor/autoload/mise.nu")
 $env.PODMAN_COMPOSE_WARNING_LOGS = false
 
 # Commands Aliases
-alias tochezmoi = cd $env.CHEZMOI_HOME
 # For container management
 alias docker = podman
 alias compose = podman compose
--- readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,9 +5,6 @@ $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = [Text.E
 Invoke-Expression (&starship init powershell)
 
 # Environment Variables
-if (where.exe chezmoi > $null 2>&1) {
-  $env:CHEZMOI_HOME = (&chezmoi source-path)
-}
 ## Mise, https://mise.jdx.dev/
 (&mise activate pwsh) | Out-String | Invoke-Expression
 ## Podman
@@ -17,10 +14,6 @@ $env:PODMAN_COMPOSE_WARNING_LOGS = $false
 # `which`: Show the path of commands
 New-Alias -Name which -Value where.exe
 New-Alias -Name touch -Value New-Item
-New-Alias -Name tochezmoi -Value To-Chezmoi
-function To-Chezmoi {
-    cd $env:CHEZMOI_HOME
-  }
 # For container management
 New-Alias -Name docker -Value podman
 function Podman-Compose {
