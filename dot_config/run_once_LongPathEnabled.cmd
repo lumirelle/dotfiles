f280876d6e9a09a543c8df6a5fccdb8ea3652710
Lumirelle <shabbyacc@outlook.com>
2026-08-24 10:53:43 +0800

    feat: use Windows built-in `sudo` instead of `gsudo`

--- dot_config/run_once_LongPathEnabled.cmd
+++ dot_config/run_once_LongPathEnabled.cmd
@@ -1,2 +1,1 @@
-@gsudo status IsElevated --no-output || (gsudo "%~f0" & exit /b)
-reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem /v LongPathsEnabled /t REG_DWORD /d 00000001
+sudo reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem /v LongPathsEnabled /t REG_DWORD /d 00000001
--- dot_config/run_onchange_disable_ctrl_space.reg.cmd
+++ dot_config/run_onchange_disable_ctrl_space.reg.cmd
@@ -1,4 +1,3 @@
 @REM {{ joinPath .chezmoi.homeDir ".config/disable_ctrl_space.reg" | include | sha256sum }}
 
-@gsudo status IsElevated --no-output || (gsudo "%~f0" & exit /b)
-reg import "%USERPROFILE%\.config\disable_ctrl_space.reg"
+sudo reg import "%USERPROFILE%\.config\disable_ctrl_space.reg"
