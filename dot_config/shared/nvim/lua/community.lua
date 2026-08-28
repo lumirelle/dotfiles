8955c457c033e27e2c3e513a2207c60c02e3dc0e
Lumirelle <shabbyacc@outlook.com>
2026-08-28 16:59:27 +0800

    feat: enable AstroVim community modules

--- dot_config/shared/nvim/lua/community.lua
+++ dot_config/shared/nvim/lua/community.lua
@@ -1,7 +1,3 @@
-if true then
-  return {}
-end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -9,6 +5,26 @@ end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.chezmoi" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.eslint" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.mdx" },
+  { import = "astrocommunity.pack.nginx" },
+  { import = "astrocommunity.pack.nushell" },
+  { import = "astrocommunity.pack.oxlint" },
+  { import = "astrocommunity.pack.pkl" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.xml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.zig" },
   -- import/override with your plugins folder
 }
