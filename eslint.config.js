35ae848a950854648af7598f05a4cc9e946c5b2d
Lumirelle <shabbyacc@outlook.com>
2026-08-31 13:47:46 +0800

    feat: switch to Microsoft.WindowsTerminal.Preview (v1.25+) for Kitty keyboard protocol support

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ export default antfu(
     type: 'lib',
     // Keep some configs style as their are,
     // to reduce the differences while updating.
-    ignores: ['**/Microsoft.WindowsTerminal_8wekyb3d8bbwe/**/*', 'dot_config/shared/clash-verge-rev/verge.yaml'],
+    ignores: ['**/Microsoft.WindowsTerminal_8wekyb3d8bbwe/**/*', '**/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/**/*', 'dot_config/shared/clash-verge-rev/verge.yaml'],
   },
   ...oxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 )
