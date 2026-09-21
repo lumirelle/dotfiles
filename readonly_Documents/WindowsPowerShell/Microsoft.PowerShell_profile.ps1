d2ae0fa77319566cab6b57fe89deea36570f2b44
Lumirelle <shabbyacc@outlook.com>
2026-09-21 15:12:48 +0800

    fix: align shells' behavior

--- readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -17,39 +17,14 @@ $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = [Text.E
 Invoke-Expression (&starship init powershell)
 ## Zoxide, https://github.com/ajeetdsouza/zoxide
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
+## Fnox, https://fnox.jdx.dev/
+Invoke-Expression (& { (fnox activate powershell | Out-String) })
 ## Podman
 $env:PODMAN_COMPOSE_WARNING_LOGS = $false
 
 # Aliases & Custom Commands
-New-Alias -Name cz -Value chezmoi
-New-Alias -Name docker -Value podman
-function Podman-Compose {
-  podman compose @args
-}
-New-Alias -Name compose -Value Podman-Compose
+New-Alias -Name lg -Value lazygit
+New-Alias -Name ch -Value chezmoi
 ## GNU flavor
 New-Alias -Name which -Value where.exe
 New-Alias -Name touch -Value New-Item
-function degit {
-  param([string]$repo, [string]$dest = ".")
-  if (Test-Path $dest -PathType Container) {
-    if ((Get-ChildItem -Path $dest | Measure-Object).Count -gt 0) {
-      Write-Error "Target directory is not empty. Please specify an empty directory or a new directory."
-      return
-    }
-  } else {
-    New-Item -Path $dest -ItemType Directory | Out-Null
-  }
-  $branch = "main"
-  if ($repo -match "#(.+)") {
-    $branch = $Matches[1]
-    $repo = $repo -replace "#.+", ""
-  }
-  $url = "https://github.com/$repo/archive/refs/heads/$branch.tar.gz"
-  Write-Host "Creating project from $url..."
-  $tempFile = New-TemporaryFile
-  curl.exe -sL -o $tempFile $url
-  tar -xzf $tempFile --strip-components=1 -C $dest
-  Remove-Item $tempFile
-  Write-Host "Done!"
-}
