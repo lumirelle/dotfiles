aec0a34f5232381490cc3190fa77d3559d21277f
Lumirelle <shabbyacc@outlook.com>
2026-09-24 14:31:52 +0800

    feat: rename script as now it disable Windows search entirely

--- dot_config/run_onchange_disable_Windows_search.cmd
+++ dot_config/run_onchange_disable_Windows_search.cmd
@@ -1,3 +1,5 @@
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v ConnectedSearchUseWeb /t REG_DWORD /d 0 /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v EnableDynamicContentInWSB /t REG_DWORD /d 0 /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v AllowCloudSearch /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v DisableWebSearch /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v DisableBackoff /t REG_DWORD /d 1 /f
